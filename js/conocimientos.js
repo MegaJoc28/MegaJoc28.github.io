d026a7c4992beefc9e2de372e1fb13dc5e27883e
MegaJoc28 <jzapatasegui@gmail.com>
2025-06-24 19:41:58 -0400

    seccion conocimientos creada con importacion y trabajando con el DOM

---
+++ js/conocimientos.js
@@ -0,0 +1,26 @@
+
+import { frontendSkills, backendSkills, otherSkills } from "./habilidades.js";
+
+function crearHabilidadHTML(skill) {
+  const div = document.createElement("div");
+  div.classList.add("conocimiento");
+
+  div.innerHTML = `
+    <img class="conocimientos-img" src="${skill.imagen}" alt="${skill.nombre}">
+    <p class="conocimientos-img__text"><span class="bold">${skill.nombre}</span> - Nivel: ${skill.estrellas} ⭐</p>
+  `;
+  
+  return div;
+}
+
+function renderizarSkills(skills, contenedorId) {
+  const contenedor = document.getElementById(contenedorId);
+  skills.forEach(skill => {
+    const skillHTML = crearHabilidadHTML(skill);
+    contenedor.appendChild(skillHTML);
+  });
+}
+
+renderizarSkills(frontendSkills, "frontend");
+renderizarSkills(backendSkills, "backend");
+renderizarSkills(otherSkills, "other");
---
+++ js/habilidades.js
@@ -0,0 +1,15 @@
+
+export const frontendSkills = [
+  { nombre: "HTML5 / CSS3", imagen: "./assets/html.png", estrellas: 4 },
+  { nombre: "JavaScript", imagen: "./assets/javascript.png", estrellas: 3 },
+];
+
+export const backendSkills = [
+  { nombre: "Node.js", imagen: "./assets/node.png", estrellas: 2 },
+  { nombre: "Express", imagen: "./assets/express.png", estrellas: 2 },
+];
+
+export const otherSkills = [
+  { nombre: "Git", imagen: "./assets/git.png", estrellas: 3 },
+  { nombre: "Figma", imagen: "./assets/figma.png", estrellas: 3 },
+];
