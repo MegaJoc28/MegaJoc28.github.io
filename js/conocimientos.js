fd406b7257a4ea43d31eb7956c5972fc7924bd89
MegaJoc28 <jzapatasegui@gmail.com>
2025-07-02 21:05:51 -0400

    manejo de errores con try...catch

--- js/conocimientos.js
+++ js/conocimientos.js
@@ -9,15 +9,19 @@ function crearHabilidadHTML(skill) {
     <img class="conocimientos-img" src="${skill.imagen}" alt="${skill.nombre}">
     <p class="conocimientos-img__text"><span class="bold">${skill.nombre}</span> - Nivel: ${skill.estrellas} ⭐</p>
   `;
-  
+
   return div;
 }
 
 function renderizarSkills(skills, contenedorId) {
   const contenedor = document.getElementById(contenedorId);
   skills.forEach(skill => {
-    const skillHTML = crearHabilidadHTML(skill);
-    contenedor.appendChild(skillHTML);
+    try {
+      const skillHTML = crearHabilidadHTML(skill);
+      contenedor.appendChild(skillHTML);
+    } catch (error) {
+      console.log(`Error al cargar:`, error);
+    }
   });
 }
 
