3e15c722bb0b6338aa52d8e1a2182e512e25dd33
MegaJoc28 <jzapatasegui@gmail.com>
2025-07-08 13:40:30 -0400

    Correcion del espacio sobrante al ocultar conocimientos.

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("conocimientos-text");
   const arrow = title.querySelector(".arrow");
 
-  title.addEventListener("click", () => {
-    const isActive = content.classList.toggle("active");
-    arrow.classList.toggle("rotated", isActive);
-  });
-
+  if (title && content && arrow) {
+    title.addEventListener("click", () => {
+      const isHidden = content.classList.toggle("hidden");
+      arrow.classList.toggle("rotated", !isHidden);
+    });
+  }
   const toggle = document.getElementById("nav-toggle");
   const menu = document.getElementById("ul-nav");
 
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    const expanded = menu.classList.contains("active");
-    toggle.setAttribute("aria-expanded", expanded);
-  });
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("active");
+      const expanded = menu.classList.contains("active");
+      toggle.setAttribute("aria-expanded", expanded);
+    });
+  }
 });
 
